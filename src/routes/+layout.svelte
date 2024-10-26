2b28c2443840a9b5b61c2bfedcb95510408d314f
smbeane5235 <smbeane5235@gmail.com>
2024-10-26 10:46:30 -0400

    updated interaction with nats

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,8 @@
 </script>
 
 <div class="flex justify-center p-10 space-x-16">
-  <a href="/sensor_map" class="btn btn-outline btn-primary btn-lg">Sensor Map</a>
-  <a href="/" class="btn btn-outline btn-primary btn-lg">Sensor Config</a>
+  <a href="/sensor-map" class="btn btn-outline btn-primary btn-lg">Sensor Map</a>
+  <a href="/lj-config" class="btn btn-outline btn-primary btn-lg">Sensor Config</a>
 </div>
 
 <slot/>
--- src/routes/nats/kv/+server.ts
+++ src/routes/nats/kv/+server.ts
@@ -1,126 +1,79 @@
-import { Kvm, KvWatchInclude } from '@nats-io/kv';
-import type { KV } from "@nats-io/kv";
-import { connect } from "@nats-io/transport-node";
+import { Kvm } from '@nats-io/kv';
+import { connect, type NatsConnection } from "@nats-io/transport-node";
 import type { RequestEvent } from "@sveltejs/kit"
+import { getKeysList, getKeyValues, watchValues, setupNats } from "$lib/nats/+server.js"
+import type { Sensor, NatsService } from "$lib/nats/+server.js"
 
 type SensorDataResponse = {
   sensorsList: string[];
   sensorValues: Sensor[] | null;
 }
-
-type Sensor = {
-  id: string;
-  name: string;
-  type: string;
-  value: string;
-}
-
-async function getKeysList(kv: KV): Promise<string[]> {
-  const sensorsList: string[] = [];
-  const keysList = await kv.keys();
-  for await(const k of keysList){
-    sensorsList.push(k)
-  }
-  return sensorsList;
-}
-
-async function getKeyValues(kvm: Kvm, sensorsList: string[]):Promise<Sensor[]>  {
-  const sensorValues: Sensor[] = [];
-    for await (const sensor of sensorsList) {
-      let kv = await kvm.open(sensor);
-      const keysList = await kv.keys();
-      let keysArray: string[] = [];
-      for await (const key of keysList){
-        keysArray.push(key)
-      }
-      let keyValues: Sensor = {
-        id: sensor,
-        name: "",
-        type: "",
-        value: "",
-      };
-      for (const key of keysArray){
-        let val = await kv.get(key)
-        keyValues[key as keyof Sensor] = val?.string() || "";
-      }
-      sensorValues.push(keyValues)
-    }
-  return sensorValues;  
-}
-
-async function watchValues(kvm: Kvm, sensorsList: string[], sensorValues: Sensor[]): Promise<Sensor[]> {
-  const stopSignal = { stopped: false };
-
-  async function watchSensor(sensor: string, i: number, stopSignal: { stopped: boolean }) {
-    const kv = await kvm.open(sensor);
-    const watch = await kv.watch({
-      include: KvWatchInclude.UpdatesOnly,
-    });
-
-    try {
-      for await (const e of watch) {
-        if (stopSignal.stopped) {
-          console.log(`Stopping watch for sensor ${i}`);
-          break; // Stop watching if another watcher has resolved
-        }
-
-        console.log(`watch: ${e.key}: ${e.operation} ${e.value ? e.string() : ""}`);
-        sensorValues[i][e.key as keyof Sensor] = e.string();
-
-        stopSignal.stopped = true; // Set the signal to stop all watches
-        break;
-      }
-    } catch (error) {
-      console.error("Error Watching Values: ", error)
-    }
-  }
-
-  // Create an array of promises for all the sensors
-  const sensorPromises = sensorsList.map((sensor, i) => watchSensor(sensor, i, stopSignal));
-  await Promise.race(sensorPromises);
-
-  return sensorValues;
-} 
-
-export async function GET({ url } : RequestEvent): Promise<Response> {
-  const type: string | null = url.searchParams.get("type");
-  try {
-    const nc = await connect({ servers: "demo.nats.io:4222" })
-    
-    const kvm = new Kvm(nc);
-    const kv = await kvm.open("sensorList");
-    const sensorsList = await getKeysList(kv);
-    let responseData: SensorDataResponse = { sensorsList, sensorValues: null };
-
-    if (type === "getKVs" || type === "watchVals"){
-      let sensorValues = await getKeyValues(kvm, sensorsList);
-      if(type === "getKVs"){
-        responseData.sensorValues = sensorValues;
-      } 
-      else{
-        console.log("Started Watching...")
-        sensorValues = await watchValues(kvm, sensorsList, sensorValues);
-        responseData = {sensorsList, sensorValues};
-      }
-    }
-
-    await nc.close();
-    return new Response( JSON.stringify(responseData), {
-      headers: { "Content-Type": "application/json"},
-      status: 200,
-    });
-  } catch (error: unknown) {
-    console.error("NATS Error: ", error);
-    return new Response(JSON.stringify({ error: (error as Error).message} ), {
-      headers: { 'Content-Type': 'application/json' },
-      status : 500});
-  }
-}
-
 type PutRequestData = {
   bucket: string;
   key: string;
   newValue: string;
+}
+type NatsConnections = [string, NatsService]
+
+const connections: NatsConnections[] = [];
+
+function connectionExists(id: string): boolean {
+  if(connections){ 
+    for(const connection of connections){
+      if(connection[0] === id){
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+export async function GET({ url } : RequestEvent): Promise<Response> {
+  const type: string | null = url.searchParams.get("type");
+  const connectionId: string | null = url.searchParams.get("connectionId");
+  if(connectionId){
+    try {
+      let nats!: NatsService;
+      if(connections == null || !connectionExists(connectionId)){
+        nats = await setupNats();
+        connections?.push([connectionId, nats])
+      } else {
+        for(const connection of connections){
+          if(connection[0] === connectionId){
+            nats = connection[1]
+          }
+        }
+      }
+      
+      let responseData: SensorDataResponse = { sensorsList: nats.bucketList, sensorValues: null };
+
+      if (type === "watchVals") {
+        console.log("Started Watching...");
+        nats.currentValues = await watchValues(nats.kvm, nats.bucketList, nats.currentValues || []);
+        responseData.sensorValues = nats.currentValues;
+      } else {
+        // Get sensor values if type is not "watchVals"
+        nats.currentValues = await getKeyValues(nats.kvm, nats.bucketList);
+        responseData.sensorValues = nats.currentValues;
+      }
+      
+      return new Response(JSON.stringify(responseData), {
+        headers: { "Content-Type": "application/json" },
+        status: 200,
+      });
+    } catch (error: unknown) {
+      console.error("NATS Error: ", error);
+      return new Response(JSON.stringify({ error: (error as Error).message }), {
+        headers: { 'Content-Type': 'application/json' },
+        status: 500
+      });
+    }
+  } else {
+    return new Response(JSON.stringify({error: "No Connection ID"}), {
+      headers: { "Content-Type": "application/json" },
+      status: 500
+    })
+  }
 }
 
 export async function PUT({request}: RequestEvent): Promise<Response> {
