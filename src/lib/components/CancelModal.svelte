acf36f3e37533066c92b1a222d4fba19e9c36cdf
Spencer Beane <smbeane5235@gmail.com>
2024-11-27 21:33:36 -0500

    added modals

---
+++ src/lib/components/CancelModal.svelte
@@ -0,0 +1,20 @@
+<script lang='ts'>
+  export let cancel_modal;
+  export let handleCancel;
+</script>
+
+<dialog id="cancel_modal" class='modal' bind:this={cancel_modal}>
+  <div class="modal-box">
+    <h3 class="text-lg font-bold">Cancel Changes?</h3>
+    <h6>Pressing 'Yes' will delete all changes made without saving</h6>
+    <div class="mt-5 flex">
+      <form method="dialog">
+        <button class="btn btn-primary">No</button>
+        <button class="btn btn-error ml-5" onclick={handleCancel}>Yes</button>
+      </form>
+    </div>
+  </div>
+  <form method="dialog" class="modal-backdrop">
+    <button>close</button>
+  </form>
+</dialog>
---
+++ src/lib/components/SaveModal.svelte
@@ -0,0 +1,21 @@
+<script lang='ts'>
+  export let save_modal: HTMLDialogElement | undefined;
+  export let saveSensorChanges;
+</script>
+
+
+<dialog id="save_modal" class='modal' bind:this={save_modal}>
+  <div class="modal-box">
+    <h3 class="text-lg font-bold">Save Changes?</h3>
+    <h6>Pressing 'Yes' will override the already saved data with the new data.</h6>
+    <div class="mt-5 flex">
+      <form method="dialog">
+        <button class="btn btn-primary">No</button>
+        <button class="btn btn-success ml-5" onclick={saveSensorChanges}>Yes</button>
+      </form>
+    </div>
+  </div>
+  <form method="dialog" class="modal-backdrop">
+    <button>close</button>
+  </form>
+</dialog>
