62b71f803122915568040ba0335b625f65bf5554
smbeane5235 <smbeane5235@gmail.com>
2025-07-28 10:23:56 -0400

    organized

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,46 +1,56 @@
 <script lang="ts">
-  import "../app.css"
-  import { goto } from "$app/navigation";
-  import { wsconnect } from "@nats-io/nats-core";
- //use session storage to save the connection ID, which will be used when in the config and map pages
-  let serverName = $state<string>("");
-  let password = $state<string>("");
-  let loading = $state<boolean>(false);
-  let alert = $state<string>("")
-  async function connect() {
-    loading = true;
-    try {
-      const server = await wsconnect({ servers : serverName})
-      if(server){
-        server.close()
-        sessionStorage.setItem("serverName", serverName)
-        goto("/config/cabinet-select");
-      }
-      loading = false;
-    } catch (error) {
-      loading = false;
-      alert="Incorrect Server Name or Password"
-      console.error("Error Initializing NATS Connection")
+    import "../app.css";
+    import { goto } from "$app/navigation";
+    import { wsconnect } from "@nats-io/nats-core";
+    //use session storage to save the connection ID, which will be used when in the config and map pages
+    let serverName = $state<string>("");
+    let password = $state<string>("");
+    let loading = $state<boolean>(false);
+    let alert = $state<string>("");
+
+    async function connect() {
+        loading = true;
+        try {
+            const server = await wsconnect({ servers: serverName });
+            if (server) {
+                server.close();
+                sessionStorage.setItem("serverName", serverName);
+                goto("/config/cabinet-select");
+            }
+            loading = false;
+        } catch (error) {
+            loading = false;
+            alert = "Incorrect Server Name or Password";
+            console.error("Error Initializing NATS Connection");
+        }
     }
-  }
 </script>
 
 <div class="flex items-center justify-center h-screen">
-  <div class="flex flex-col justify-center text-center items-center">
-    <h1 class="text-4xl">Welcome to AvenaOTR!</h1>
-    <h2 class="text-2xl pt-5 pb-10">Enter Server Credentials Below:</h2>
-    <h2 class="text-red-600 pb-5">{alert}</h2>
-    <input type="text" placeholder="Server" bind:value={serverName} class="input input-bordered input-primary  w-72 bg-secondary text-accent placeholder-accent mb-3"/>
-    <input type="text" placeholder="Password" bind:value={password} class="input input-bordered input-primary w-72 bg-secondary text-accent placeholder-accent mb-3"/>
-    <button class="btn btn-secondary w-28 mb-20" onclick={connect}>
-      {#if loading}
-      <div class="loading-overlay">
-        <span class="loading loading-spinner loading-lg"></span>  
-      </div>
-      {:else}
-        Connect
-      {/if}
-    </button>
-  </div>
+    <div class="flex flex-col justify-center text-center items-center">
+        <h1 class="text-4xl">Welcome to AvenaOTR!</h1>
+        <h2 class="text-2xl pt-5 pb-10">Enter Server Credentials Below:</h2>
+        <h2 class="text-red-600 pb-5">{alert}</h2>
+        <input
+            type="text"
+            placeholder="Server"
+            bind:value={serverName}
+            class="input input-bordered input-primary w-72 bg-secondary text-accent placeholder-accent mb-3"
+        />
+        <input
+            type="text"
+            placeholder="Password"
+            bind:value={password}
+            class="input input-bordered input-primary w-72 bg-secondary text-accent placeholder-accent mb-3"
+        />
+        <button class="btn btn-secondary w-28 mb-20" onclick={connect}>
+            {#if loading}
+                <div class="loading-overlay">
+                    <span class="loading loading-spinner loading-lg"></span>
+                </div>
+            {:else}
+                Connect
+            {/if}
+        </button>
+    </div>
 </div>
-
--- src/routes/config/cabinet-select/+page.svelte
+++ src/routes/config/cabinet-select/+page.svelte
@@ -1,81 +1,94 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import { NatsService, connect, getKeys, getKeyValue } from "$lib/nats.svelte";
-  import { goto } from "$app/navigation";
+    import { onMount } from "svelte";
+    import {
+        NatsService,
+        connect,
+        getKeys,
+        getKeyValue,
+    } from "$lib/nats.svelte";
+    import { goto } from "$app/navigation";
 
-  type Cabinet = {
-  "id": string
-  "status": string
-  };
-
-  let serverName: string | null;
-  let nats: NatsService | null;
-  let cabinetKeys = $state<string[]>([])
-  let cabinets = $state<Cabinet[]>([]);
-  let loading = $state<boolean>(true);
-
-  //initializes the nats connection and gets all cabinet vals
-  async function initialize() {
-    if(serverName) nats = await connect(serverName)
-    if(nats) {
-      let keys = await getKeys(nats, "all_cabinets");
-      for (const key of keys) {
-        let values = await getCabinet("all_cabinets", key);
-        cabinets.push(values);
-        console.log(values);
-      }
-      loading = false;
-    } else {
-      console.log('No Nats Connection');
-    }
-  }
-
-  //gets the value of one cabinet
-  async function getCabinet(bucket: string, key: string): Promise<Cabinet>{
-    if(!nats) throw new Error("Nats connection is not initialized");
-    let cabinet = {
-      "id": key,
-      "status": ""
+    type Cabinet = {
+        id: string;
+        status: string;
     };
-    let status = await getKeyValue(nats, bucket, key);
-    cabinet.status = JSON.parse(status).status
-    return cabinet;
-  }
 
-  //once selected, sets the selectedCabinet to session storage and redirects to labjack config page
-  function selectConfig(selectedCabinet: string) {
-    sessionStorage.setItem("selectedCabinet", selectedCabinet)
-    goto("/config/lj-config");
-  }
+    let serverName: string | null;
+    let nats: NatsService | null;
+    let cabinetKeys = $state<string[]>([]);
+    let cabinets = $state<Cabinet[]>([]);
+    let loading = $state<boolean>(true);
 
-  //gets the server name from session storage & initalizes
-  onMount(() => {
-    serverName = sessionStorage.getItem("serverName")
-    initialize();
-  })
+    //initializes the nats connection and gets all cabinet vals
+    async function initialize() {
+        if (serverName) nats = await connect(serverName);
+        if (nats) {
+            let keys = await getKeys(nats, "all_cabinets");
+            for (const key of keys) {
+                let values = await getCabinet("all_cabinets", key);
+                cabinets.push(values);
+                console.log(values);
+            }
+            loading = false;
+        } else {
+            console.log("No Nats Connection");
+        }
+    }
+
+    //gets the value of one cabinet
+    async function getCabinet(bucket: string, key: string): Promise<Cabinet> {
+        if (!nats) throw new Error("Nats connection is not initialized");
+        let cabinet = {
+            id: key,
+            status: "",
+        };
+        let status = await getKeyValue(nats, bucket, key);
+        cabinet.status = JSON.parse(status).status;
+        return cabinet;
+    }
+
+    //once selected, sets the selectedCabinet to session storage and redirects to labjack config page
+    function selectConfig(selectedCabinet: string) {
+        sessionStorage.setItem("selectedCabinet", selectedCabinet);
+        goto("/config/lj-config");
+    }
+
+    //gets the server name from session storage & initalizes
+    onMount(() => {
+        serverName = sessionStorage.getItem("serverName");
+        initialize();
+    });
 </script>
 
 {#if loading}
-  <div class="loading-overlay">
-    <span class="loading loading-spinner loading-lg"></span>  
-  </div>
-{:else if cabinets !== null && cabinetKeys !== null}
-  <div class="flex flex-col items-center w-full px-10">
-    <h1>Select Roadside Cabinet</h1>
-    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 w-full max-w-[80%]">
-      {#each cabinets as cabinet}
-      <div class="card bg-primary shadow-xl text-neutral p-4">
-        <div class="card-body space-y-4">
-          <h2 class="card-title text-center">{cabinet["id"]}</h2>
-          <p class="pl-2 mt-2"><strong>Status:</strong> {cabinet.status}</p>
-          <div class="mt-3 flex justify-center">
-            <button class="btn btn-outline btn-success" onclick={() =>selectConfig(cabinet.id)}>
-              Select Cabinet
-            </button>
-          </div>
-        </div>
-      </div>
-      {/each}
+    <div class="loading-overlay">
+        <span class="loading loading-spinner loading-lg"></span>
     </div>
-  </div>
+{:else if cabinets !== null && cabinetKeys !== null}
+    <div class="flex flex-col items-center w-full px-10">
+        <h1>Select Roadside Cabinet</h1>
+        <div
+            class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 w-full max-w-[80%]"
+        >
+            {#each cabinets as cabinet}
+                <div class="card bg-primary shadow-xl text-neutral p-4">
+                    <div class="card-body space-y-4">
+                        <h2 class="card-title text-center">{cabinet["id"]}</h2>
+                        <p class="pl-2 mt-2">
+                            <strong>Status:</strong>
+                            {cabinet.status}
+                        </p>
+                        <div class="mt-3 flex justify-center">
+                            <button
+                                class="btn btn-outline btn-success"
+                                onclick={() => selectConfig(cabinet.id)}
+                            >
+                                Select Cabinet
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            {/each}
+        </div>
+    </div>
 {/if}
