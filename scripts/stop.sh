8dfb28f9544d2aa31ed2f132448ce5ece82f7c30
Anugunj Naman <anugunjjha@gmail.com>
2026-01-30 13:30:10 -0500

    fixes

--- scripts/stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-APP_NAME="avena-rs"
-
-echo ">>> Stopping app..."
-pm2 stop "$APP_NAME" || echo "App not running"
-pm2 delete "$APP_NAME" || echo "App already deleted"
-pm2 save
-
-echo ">>> App shut down. You can now edit or rebuild manually."
